50d7c3dc5594e0e4c6a19ebbf649510d567f3777
camilof <camilofetecua2011@outlook.com>
2024-07-29 18:21:08 -0500

    add web page

---
+++ assets/img/experience_plugin_mancii.js
@@ -0,0 +1,215 @@
+
+    const miniDom = {
+  "tagName": "div",
+  "attributes": {
+    "class": "canvas__content canvas__content--active",
+    "id": "tab-content-Mobile",
+    "style": "width: 360px;",
+    "data-astro-cid-j7pv25f6": "",
+    "data-astro-source-file": "/home/user/web_experience_creator/src/pages/index.astro",
+    "data-astro-source-loc": "228:78"
+  },
+  "children": [
+    {
+      "tagName": "h1",
+      "attributes": {
+        "id": "95e3abec-2d36-4f77-b02f-76708dac5538",
+        "data-element-type": "h1",
+        "data-style": "5afd3d01-b227-44d6-815e-b0462fceddfb"
+      },
+      "children": [
+        {
+          "tagName": "TextNode",
+          "attributes": {},
+          "children": [],
+          "styles": {},
+          "events": [],
+          "className": {},
+          "rules": {},
+          "styleId": "",
+          "textContent": "Título H1"
+        }
+      ],
+      "styles": {},
+      "events": [],
+      "className": {},
+      "rules": {
+        "clases": [
+          "[data-style=\"5afd3d01-b227-44d6-815e-b0462fceddfb\"] { }"
+        ]
+      },
+      "styleId": "5afd3d01-b227-44d6-815e-b0462fceddfb",
+      "textContent": ""
+    },
+    {
+      "tagName": "h3",
+      "attributes": {
+        "id": "14c9f5d7-2f3c-47f9-b86a-06749738bbb0",
+        "data-element-type": "h3",
+        "data-style": "bc3a6dbc-f51a-4d72-bd6a-d7e20b03f629"
+      },
+      "children": [
+        {
+          "tagName": "TextNode",
+          "attributes": {},
+          "children": [],
+          "styles": {},
+          "events": [],
+          "className": {},
+          "rules": {},
+          "styleId": "",
+          "textContent": "Título H3"
+        }
+      ],
+      "styles": {},
+      "events": [],
+      "className": {},
+      "rules": {
+        "clases": [
+          "[data-style=\"bc3a6dbc-f51a-4d72-bd6a-d7e20b03f629\"] { }"
+        ]
+      },
+      "styleId": "bc3a6dbc-f51a-4d72-bd6a-d7e20b03f629",
+      "textContent": ""
+    },
+    {
+      "tagName": "iframe",
+      "attributes": {
+        "id": "5f4d615c-025d-4372-9d44-c8e303b386d5",
+        "src": "https://mancii.com/",
+        "width": "320",
+        "height": "240",
+        "data-element-type": "iframe",
+        "data-style": "0c8e07f2-d2d7-4f56-9f97-7cd5ee59aa63"
+      },
+      "children": [],
+      "styles": {},
+      "events": [],
+      "className": {},
+      "rules": {
+        "clases": [
+          "[data-style=\"0c8e07f2-d2d7-4f56-9f97-7cd5ee59aa63\"] { font-size: calc(var(--font-size-scale,1)*var(--font-size-unit,1px)); letter-spacing: calc(var(--letter-spacing-scale,1)*var(--letter-spacing-unit,1px)); line-height: calc(var(--line-height-scale,1)*var(--line-height-unit,1px)); text-shadow: var(--text-shadow-offset-x,0px)var(--text-shadow-offset-y,0px)var(--text-shadow-blur,0px)var(--text-shadow-color,#000); width: calc(var(--width-scale)*var(--width-unit,1px)); height: calc(var(--height-scale)*var(--height-unit,1px)); margin-top: calc(var(--margin-top-scale,0)*var(--margin-top-unit,1px)); margin-bottom: calc(var(--margin-bottom-scale,0)*var(--margin-bottom-unit,1px)); margin-left: calc(var(--margin-left-scale,0)*var(--margin-left-unit,1px)); margin-right: calc(var(--margin-right-scale,0)*var(--margin-right-unit,1px)); padding-top: calc(var(--padding-top-scale,0)*var(--padding-top-unit,1px)); padding-right: calc(var(--padding-right-scale,0)*var(--padding-right-unit,1px)); padding-bottom: calc(var(--padding-bottom-scale,0)*var(--padding-bottom-unit,1px)); padding-left: calc(var(--padding-left-scale,0)*var(--padding-left-unit,1px)); top: calc(var(--top-scale,0)*var(--top-unit,1px)); left: calc(var(--left-scale,0)*var(--left-unit,1px)); right: calc(var(--right-scale,0)*var(--right-unit,1px)); bottom: calc(var(--bottom-scale,0)*var(--bottom-unit,1px)); transform: scale3d(calc(var(--transform-scale-x,1)*var(--transform-scale-x-unit,1)),calc(var(--transform-scale-y,1)*var(--transform-scale-y-unit,1)),calc(var(--transform-scale-z,1)*var(--transform-scale-z-unit,1)))translate3d(calc(var(--transform-translate-x,0)*var(--transform-translate-x-unit,1px)),calc(var(--transform-translate-y,0)*var(--transform-translate-y-unit,1px)),calc(var(--transform-translate-z,0)*var(--transform-translate-z-unit,1px)))rotateX(calc(var(--transform-rotate-x,0)*var(--transform-rotate-x-unit,1deg)))rotateY(calc(var(--transform-rotate-y,0)*var(--transform-rotate-y-unit,1deg)))rotateZ(calc(var(--transform-rotate-z,0)*var(--transform-rotate-z-unit,1deg)))skew(calc(var(--transform-skew-x,0)*var(--transform-skew-x-unit,1deg)),calc(var(--transform-skew-y,0)*var(--transform-skew-y-unit,1deg)))perspective(calc(var(--transform-perspective,1000px)*var(--transform-perspective-unit,1))); transform-origin: calc(var(--transform-origin-x,50)*1%)calc(var(--transform-origin-y,50)*1%); max-width: calc(var(--max-width-scale)*var(--max-width-unit,1px)); max-height: calc(var(--max-height-scale)*var(--max-height-unit,1px)); min-width: calc(var(--min-width-scale)*var(--min-width-unit,1px)); min-height: calc(var(--min-height-scale)*var(--min-height-unit,1px)); border-radius: calc(var(--border-radius-scale,1)*var(--border-radius-unit,1px)); border-width: calc(var(--border-width-scale,1)*var(--border-width-unit,1px)); border-color: var(--border-color-hex,#000000); box-shadow: var(--shadow-offset-x,0px)var(--shadow-offset-y,0px)var(--shadow-blur,0px)var(--shadow-spread,0px)var(--shadow-color,#000); filter: hue-rotate(calc(var(--hue-scale,1)*var(--hue-unit,1deg))); --width-scale: 600; --height-scale: 700; }"
+        ]
+      },
+      "styleId": "0c8e07f2-d2d7-4f56-9f97-7cd5ee59aa63",
+      "textContent": ""
+    },
+    {
+      "tagName": "input",
+      "attributes": {
+        "type": "email",
+        "id": "4fdd92b7-aeb9-4a0b-a5ad-00a732e8ec34",
+        "placeholder": "nombre@ejemplo.com",
+        "data-element-type": "input",
+        "data-style": "9154eed8-11e1-45a0-b358-1fd167509ff5"
+      },
+      "children": [],
+      "styles": {},
+      "events": [],
+      "className": {},
+      "rules": {
+        "clases": [
+          "[data-style=\"9154eed8-11e1-45a0-b358-1fd167509ff5\"] { }"
+        ]
+      },
+      "styleId": "9154eed8-11e1-45a0-b358-1fd167509ff5",
+      "textContent": ""
+    },
+    {
+      "tagName": "input",
+      "attributes": {
+        "type": "password",
+        "id": "e3f591e0-38ec-428d-adca-dd48896ba41b",
+        "placeholder": "Contraseña segura",
+        "data-element-type": "input",
+        "data-style": "7876ef81-9d27-45bf-ab38-4b4a154fc17e"
+      },
+      "children": [],
+      "styles": {},
+      "events": [],
+      "className": {},
+      "rules": {
+        "clases": [
+          "[data-style=\"7876ef81-9d27-45bf-ab38-4b4a154fc17e\"] { }"
+        ]
+      },
+      "styleId": "7876ef81-9d27-45bf-ab38-4b4a154fc17e",
+      "textContent": ""
+    },
+    {
+      "tagName": "p",
+      "attributes": {
+        "id": "c3c17f68-dfdd-42b2-8155-bfb6dbd9fba6",
+        "data-element-type": "p",
+        "data-style": "ad81f384-7b12-4b4a-816d-1a518847ceb6"
+      },
+      "children": [
+        {
+          "tagName": "TextNode",
+          "attributes": {},
+          "children": [],
+          "styles": {},
+          "events": [],
+          "className": {},
+          "rules": {},
+          "styleId": "",
+          "textContent": "Texto del párrafo"
+        }
+      ],
+      "styles": {},
+      "events": [],
+      "className": {},
+      "rules": {
+        "clases": [
+          "[data-style=\"ad81f384-7b12-4b4a-816d-1a518847ceb6\"] { }"
+        ]
+      },
+      "styleId": "ad81f384-7b12-4b4a-816d-1a518847ceb6",
+      "textContent": ""
+    }
+  ],
+  "styles": {},
+  "events": [],
+  "className": {},
+  "rules": {},
+  "textContent": ""
+};
+    //aqui va la funcion de la guia,  lo que haga aqui, sera Javascript, aqui no hay tipado.
+    const createElementFromMiniDOM = (miniElement) => {
+  const element = document.createElement(miniElement.tagName);
+  for (const [attrName, attrValue] of Object.entries(miniElement.attributes)) {
+    element.setAttribute(attrName, attrValue);
+  }
+  for (const event of miniElement.events) {
+    element.addEventListener(event.type, event.handler);
+  }
+  if (miniElement.tagName === "TextNode") {
+    element.textContent = miniElement.textContent;
+  } else {
+    for (const child of miniElement.children) {
+      const childElement = createElementFromMiniDOM(child);
+      element.appendChild(childElement);
+    }
+  }
+  const styleElement = document.createElement("style");
+  const cssRules = Object.values(miniElement.rules).flat().join("\n");
+  styleElement.innerHTML = cssRules;
+  document.head.appendChild(styleElement);
+  return element;
+}
+    const restoreDOM = (miniDOM, containerSelector) => {
+  const container = document.querySelector(containerSelector);
+  if (!container) {
+    console.error(`No se encontró el contenedor con el selector: ${containerSelector}`);
+    return;
+  }
+  container.innerHTML = "";
+  const rootElement = createElementFromMiniDOM(miniDOM);
+  container.appendChild(rootElement);
+}
+    
+        document.querySelector('undefined').addEventListener('undefined', () => {
+          restoreDOM(miniDom, "body");
+        });
+      
+    
